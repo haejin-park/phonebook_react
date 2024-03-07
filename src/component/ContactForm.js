7aec8ca6063ae29c30e2dbdfb364446a188d00cc
haejin <luckyhaejin1126@gmail.com>
2024-03-08 01:21:54 +0900

    추가하고 이름, 전화번호 빈값 되게

--- src/component/ContactForm.js
+++ src/component/ContactForm.js
@@ -12,17 +12,19 @@ const ContactForm = () => {
       type: "ADD_CONTACT",
       payload: { name, phoneNumber }
     });
+    setName('');
+    setPhoneNumber('');
   }
   return (
     <div>
       <Form onSubmit={addContact}>
       <Form.Group className="mb-3" controlId="formName">
         <Form.Label>이름</Form.Label>
-        <Form.Control type="text" placeholder="이름을 입력해주세요" onChange={(e) => setName(e.target.value)} />
+        <Form.Control type="text" value={name} placeholder="이름을 입력해주세요" onChange={(e) => setName(e.target.value)} />
       </Form.Group>
       <Form.Group className="mb-3" controlId="formPhoneNumber">
         <Form.Label>전화번호</Form.Label>
-        <Form.Control type="number" placeholder="전화번호를 입력해주세요" onChange={(e) => setPhoneNumber(e.target.value)} />
+        <Form.Control type="number" value={phoneNumber} placeholder="전화번호를 입력해주세요" onChange={(e) => setPhoneNumber(e.target.value)} />
       </Form.Group>
       <Button variant="primary" type="submit">
         추가
